f0b5d7e887d52dd1423a9fe05e496f84bd2500be
lovremitrovic <lovre.mitrovic@fer.hr>
2022-12-14 18:43:14 +0100

    Revert "Initial commit"
    This reverts commit 930379c73dfc7c3a4c5a137572cbc15c2a52ec83.

--- app/db/NotFoundDatabaseError.js
+++
@@ -1,8 +0,0 @@
-class NotFoundDatabaseError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "ValidationError";
-    }
-}
-
-module.exports = NotFoundDatabaseError;
--- app/middlewares/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-function isLogged(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    //res.status(401).send('Not Logged In');
-    res.render('index',{auth:false,user:null})
-}
-
-module.exports = isLogged
