66e2cfb28ee3daad2ed72e9cf39903749ea90cf6
lovremitrovic <lovre.mitrovic@fer.hr>
2023-01-06 02:35:36 +0100

    Remove redundancy

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -2,13 +2,7 @@ const db = require("../db/db");
 const like = (req, res) => {
     let like = req.body.value;//boolean
     let label = req.params.label;
-    let isLabelValid = label === 'track' || label === 'artist' || label === 'album';
     const id = req.params.id;
-    /*
-    if(!isLabelValid){
-        res.status(400).send('Value param can only be true or false. Label param can only be track, artist or album');
-        return
-    }*/
     const pUser = db.first('user','fid', req.user.fid);
     const pEntity = db.findById(label,id);//can be track artist or album
 
